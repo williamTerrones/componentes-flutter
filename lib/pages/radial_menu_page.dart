329d00ee557a343ad83ac55ff1ee1c4512a9779d
William Terrones <william.terrones@outlook.com>
2021-01-10 00:33:42 -0500

    [Add] radial menu

--- lib/pages/radial_menu_page.dart
+++ lib/pages/radial_menu_page.dart
@@ -1,13 +1,42 @@
+import 'package:fl_radial_menu/fl_radial_menu.dart';
 import 'package:flutter/material.dart';
 
 
 class RadialMenuPage extends StatelessWidget {
 
+  final items = [
+    RadialMenuItem(
+      Icon(Icons.camera), 
+      Colors.red, 
+      () => {},
+    ),
+    RadialMenuItem(
+      Icon(Icons.calendar_today), 
+      Colors.green, 
+      () => {},
+    ),
+    RadialMenuItem(
+      Icon(Icons.camera), 
+      Colors.red, 
+      () => {},
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bubled Navigation Bar"),
+        title: Text("Radial Menu Page"),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            RadialMenu(items, fanout: Fanout.bottomRight,),
+            RadialMenu(items, fanout: Fanout.bottom,),
+            RadialMenu(items, fanout: Fanout.bottomLeft,),
+            RadialMenu(items, fanout: Fanout.top,),
+          ],
+        ),
       ),
     );
   }
